97330a73d38f2a082cb4561a3c37b60934f9b81a
Lenovo <104priyanshu@gmail.com>
2024-03-06 21:16:01 +0530

    new add

--- app/src/main/java/com/bypriyan/togocartpro/MainActivity.kt
+++ app/src/main/java/com/bypriyan/togocartpro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bypriyan.togocartpro
 
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +11,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.bypriyan.togocartpro.ui.theme.ToGoCartProTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Hello Android!")
                 }
             }
         }
@@ -33,15 +41,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "$name",
+        color = colorResource(R.color.appColor),
+        fontWeight = FontWeight.Black,
+        fontSize = 30.sp,
+        textAlign = TextAlign.Right,
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ToGoCartProTheme {
-        Greeting("Android ")
+        Greeting("Hello Android !")
     }
 }
