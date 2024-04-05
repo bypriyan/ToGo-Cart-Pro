051aa2a9ead1cd34fc1ccf176c5d867fb99d6e24
chintu  kumar das <162434759+ChintuKumar111@users.noreply.github.com>
2024-04-05 10:24:04 +0530

    status bar change color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.48.1")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation ("com.airbnb.android:lottie:6.4.0")
 
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
--- app/src/main/java/com/bypriyan/togocartpro/register/LoginActivity.kt
+++ app/src/main/java/com/bypriyan/togocartpro/register/LoginActivity.kt
@@ -3,44 +3,65 @@ package com.bypriyan.togocartpro.register
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.bypriyan.togocartpro.register.ui.theme.ToGoCartProTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.bypriyan.togocartpro.R
+
+
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToGoCartProTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
-                }
+
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                loginScreen()
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ToGoCartProTheme {
-        Greeting2("Android")
+fun loginScreen() {
+ Column (modifier = Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally){
+
+            Image(painter = painterResource(id = R.drawable.login_screen_grocery_image), contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .align(Alignment.CenterHorizontally),
+                contentScale = ContentScale.Crop
+            )
+     OutlinedTextField(value = "", onValueChange ={}, label = {
+         Text(text = "Phone Number")
+     } )
     }
+
 }
+
